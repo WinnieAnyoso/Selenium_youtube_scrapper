10abddace3bfe3fc8e91d63bdd7dd5209314370a
anyosow <64724048+WinnieAnyoso@users.noreply.github.com>
2021-11-30 16:30:06 +0000

    Created scratch.py

--- scratch.py
+++ scratch.py
@@ -20,4 +20,4 @@ print(f'found {len(video_divs)} videos')
 
 #beautiful contains no videos at this point on the initial page. so we have to simulate a headless browser that runs all the javascript on the page. Selenium comes into the picture. i.e. Selenium Automates browsers
 
-#Moved from line 5 to scratch.py
+#Moved code from scrapper to scratch.py
