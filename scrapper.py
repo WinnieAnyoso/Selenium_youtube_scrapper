9f40fa5f951bea8d9f30d3d272c01522f27bbb32
anyosow <64724048+WinnieAnyoso@users.noreply.github.com>
2021-12-02 05:53:13 +0000

    updated package configurations for selenium

--- scrapper.py
+++ scrapper.py
@@ -3,3 +3,7 @@ from selenium import webdriver
 Youtube_trending_url = "https://www.youtube.com/feed/trending"
 
 driver = webdriver.Chrome()
+
+driver.get(Youtube_trending_url)
+
+print('Page title :', driver.title)
